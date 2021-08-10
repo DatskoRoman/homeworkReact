07a6c3e7756828aacc7b5033c5314c2ea7f403bf
DatskoRoman <pbk.datsko@gmail.com>
2021-08-10 14:40:24 +0300

    delete homework, create classwork

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import Character from "./components/character/Character";
 
 function App() {
   return (
     <div>
-    <Character/>
     </div>
   );
 }
