e24afc71643805cd19fa15512d1d6a295ab0c5c9
DatskoRoman <pbk.datsko@gmail.com>
2021-08-10 14:48:04 +0300

    modified homework  1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Character from "./components/character/Character";
 
 function App() {
   return (
-    <div>
-    </div>
+      <div>
+        <Character/>
+      </div>
   );
 }
 
 export default App;
+
