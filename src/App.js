4074c21d878087fc56249bd3c0a9f2058ef2831b
DatskoRoman <pbk.datsko@gmail.com>
2021-08-19 16:15:48 +0300

    homework lesson 5 - 19.08

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from"react-router-dom";
 
 import './App.css';
+import EvenCar from "./components/EvenCar";
+import OddCars from "./components/OddCars";
 
 function App() {
   return (
-    <div></div>
+      <Router>
+        <div className={'styleGref'}>
+          <div className={'styleLink'}>
+            <Link to={'/EvenCar'}>Even Cars</Link>
+          </div>
+            <br/>
+          <div className={'styleLink'}>
+            <Link to={'/OddCars'}>Odd Cars</Link>
+          </div>
+        </div>
+        <div>
+          <Switch>
+            <Route path={'/EvenCar'} component={EvenCar}/>
+            <Route path={'/OddCars'} component={OddCars}/>
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
---
+++ src/components/EvenCar.js
@@ -0,0 +1,28 @@
+import {useEffect, useState} from "react";
+
+export default function EvenCar() {
+    let [users, setUsers] = useState([])
+    let filter = users.filter(value => value.model.length %2 === 0)
+    console.log(filter)
+    useEffect(()=>{
+
+        fetch('http://195.72.146.25/api/v1/cars')
+            .then(value => value.json())
+            .then(value => {
+                setUsers([...value]);
+            })
+    },[])
+    return (
+        <div>
+            <h1>Even Cars</h1>
+            {
+                filter.map(value =>
+                    <div className={'styleUsers'} key={value.id}>
+                        <h2>Id: {value.id}. Model: {value.model}; Price: {value.price}; Year: {value.year};</h2>
+                    </div>
+                )
+            }
+        </div>
+    )
+
+}
