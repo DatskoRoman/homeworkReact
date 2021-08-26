86906a1813f9c569e68287d2d26c670cc053f75a
DatskoRoman <pbk.datsko@gmail.com>
2021-08-26 12:59:20 +0300

    lesson 7 classwork

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import {useReducer} from "react";
 import reducer from "./reducer/Reducer";
 
 function App() {
-  let [{a}, dispatch] = useReducer(reducer, {a: 0});
+  let [{a, b, c}, dispatch] = useReducer(reducer, {a: 0, b: 0, c: 0});
   return (
     <div>
       <h1>State 1 - {a}</h1>
       <button onClick={()=> dispatch({obj: '1', diya: '+'})}>inc</button>
       <button onClick={()=> dispatch({obj: '1', diya: '-'})}>inc</button>
+        <h1>State 2 - {b}</h1>
+        <button onClick={()=> dispatch({obj: '2', diya: '+'})}>inc</button>
+        <button onClick={()=> dispatch({obj: '2', diya: '-'})}>inc</button>
+        <h1>State 3 - {c}</h1>
+        <button onClick={()=> dispatch({obj: '3', diya: '+'})}>inc</button>
+        <button onClick={()=> dispatch({obj: '3', diya: '-'})}>inc</button>
     </div>
   );
 }
--- src/reducer/Reducer.js
+++ src/reducer/Reducer.js
@@ -1,10 +1,17 @@
 const reducer = (state, action) => {
     if (action.obj === '1' && action.diya === '+'){
-        return {...state, a: state.a+10}
+        return {...state, a: state.a++}
     }else if (action.obj === '1' && action.diya === '-'){
-        return {...state, a: state.a-2}
-    }
-    return {...state};
+        return {...state, a: state.a--}
+    }else if (action.obj === '2' && action.diya === '+'){
+        return {...state, b: state.b+5}
+    }else if (action.obj === '2' && action.diya === '-'){
+        return {...state, b: state.b-10}
+    }else if (action.obj === '3' && action.diya === '+'){
+        return {...state, c: state.c+10}
+    }else if (action.obj === '3' && action.diya === '-'){
+        return {...state, c: state.c-5}
+    return {...state};}
 }
 
 export default reducer;
