e2962c9129de92cb2a4f30c16258d805a3d672bf
DatskoRoman <pbk.datsko@gmail.com>
2021-09-02 19:28:01 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
-import Character from "./components/character/Character";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import alertify from 'alertifyjs';
+import Home from "./components/Home/Home";
+import {addLocalStorage, removeMovieFromStorage} from "./Functions/StorageFunctions/storageFunctions";
+import ScrollTop from "./components/elements/ScrollTop/ScrollTop";
+import Navi from "./components/Navi/Navi";
+import FavouriteMovies from "./components/FavouriteMovies/FavouriteMovies";
+import Movie from "./components/Movie/Movie";
+import PersonInfo from "./components/PersonInfo/PersonInfo";
+import NotFound from "./components/NotFound/NotFound";
 
-function App() {
-  return (
-    <div></div>
-  );
+class App extends Component {
+
+    state = {
+        favouriteMovies: [],
+    }
+
+    componentDidMount() {
+        if (localStorage.getItem("favouriteMovies")) { // First Contact this App 
+            let movies = JSON.parse(localStorage.getItem('favouriteMovies'));
+            this.setState({
+                ...this.state,
+                favouriteMovies : movies,
+            })
+        }
+    }
+
+    getFavouriteMovies = favouritesMovie => {
+        const stateMovies = this.state.favouriteMovies;
+        const addedMovie = stateMovies.find(movie => movie.id === favouritesMovie.id);
+        if (!addedMovie) {
+            this.setState({
+                ...this.state,
+                favouriteMovies: [...stateMovies, favouritesMovie],
+            })
+            addLocalStorage(favouritesMovie)
+            alertify.success("Film Başarı İle Eklendi", 2)
+        }
+        else {
+            alertify.error("Bu film zaten listenizde bulunuyor", 2)
+
+        }
+    }
+
+    clearFavouriteMovie = id => {
+        const filterMovie = this.state.favouriteMovies.filter(movie => movie.id !== id);
+        this.setState({
+            favouriteMovies: filterMovie
+        })
+        alertify.success(" Bu Film Başarı İle Silindi", 2)
+        removeMovieFromStorage(id)
+    }
+
+    clearAllFavouriteMovies = () => {
+        this.setState({
+            favouriteMovies: []
+        })
+        if (this.state.favouriteMovies.length) {
+            alertify.success(" Bütün Filmler Başarı İle Silindi", 2)
+        }
+
+        localStorage.removeItem("favouriteMovies");
+    }
+
+
+    render() {
+
+        const { favouriteMovies } = this.state;
+
+        return (
+            <Router>
+                <ScrollTop>
+                    <React.Fragment>
+                        <Navi />
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route exact path="/favourites"
+                                   render = {
+                                       props => (
+                                           <FavouriteMovies
+                                               {...props}
+                                               favouriteMovies={favouriteMovies}
+                                               clearAllFavouriteMovies={this.clearAllFavouriteMovies}
+                                               clearFavouriteMovie={this.clearFavouriteMovie}
+                                           />
+                                       )
+                                   }
+                            />
+                            <Route exact path="/movie/:movieId"
+                                   render={
+                                       props => (
+                                           <Movie
+                                               {...props}
+                                               getFavouriteMovies={this.getFavouriteMovies}
+                                           />
+                                       )
+                                   }
+                            />
+                            <Route exact path="/person/:personId" component={PersonInfo} />
+                            <Route component={NotFound} />
+                        </Switch>
+                    </React.Fragment>
+                </ScrollTop>
+            </Router>
+        )
+    }
+
 }
-
 export default App;
---
+++ src/components/elements/HaveNotFavouriteMovies/HaveNotFavouriteMovies.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HaveNotFavouriteMovies.css";
+
+
+const HaveNotFavouriteMovies = () => {
+    return (
+        <div className="text-container text-center">
+            <img />
+            <h1>Favorilere Film Eklemek İçin ♥ İkonuna Tıklayınız. </h1>
+        </div>
+    );
+};
+
+export default HaveNotFavouriteMovies;
